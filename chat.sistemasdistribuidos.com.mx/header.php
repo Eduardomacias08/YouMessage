be667c227fa4bace8dbd87c8ae8058f3b932540f
Eduardomacias08 <93840060+Eduardomacias08@users.noreply.github.com>
2021-11-06 13:52:17 -0600

    Add files via upload

---
+++ chat.sistemasdistribuidos.com.mx/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>CHAT</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="desing/css/general.css">
+    <link rel="stylesheet" href="desing/css/nav-lateral.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+
+</head>
---
+++ chat.sistemasdistribuidos.com.mx/php/config.php
@@ -0,0 +1,20 @@
+<?php
+  // $hostname = "108.167.146.53";
+  // $username = "unevebsy_mike";
+  // $password = "martinez6143.";
+  // $dbname = "unevebsy_sise_data_uneven";
+
+  // $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  // if(!$conn){
+  //   echo "Database connection error".mysqli_connect_error();
+  // }
+  $hostname = "162.241.62.190";
+  $username = "siste200";
+  $password = "SLa#7Z92]4alOz";
+  $dbname = "siste200_chat";
+
+  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  if(!$conn){
+    echo "Database connection error".mysqli_connect_error();
+  }
+?>
